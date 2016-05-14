00ccea41cb0c19a9f39b046951806be910d4dc28
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-05-14 10:42:54 -0700

    create env

--- server/env/index.js
+++ server/env/index.js
@@ -1,2 +1,9 @@
+var path = require('path');
+var devConfigPath = path.join(__dirname, './development.js');
+var productionConfigPath = path.join(__dirname, './production.js');
 
-var env = require(path.join(rootPath, './server/env'));
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require(productionConfigPath);
+} else {
+    module.exports = require(devConfigPath);
+}
