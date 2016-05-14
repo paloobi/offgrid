399cd295a23829a406d0e3499e5e530d2f5b0349
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-05-14 10:58:32 -0700

    building db in

--- server/env/development.js
+++ server/env/development.js
@@ -2,8 +2,8 @@ module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/offgrid-dev",
   "SESSION_SECRET": "All your base are belong to us",
   "FACEBOOK": {
-    "clientID": "",
-    "clientSecret": "",
+    "clientID": "1189623257722728",
+    "clientSecret": "13c40d6d5551dd1af6ba6805f7f68dfa",
     "callbackURL": "/auth/facebook/callback"
   }
 };
